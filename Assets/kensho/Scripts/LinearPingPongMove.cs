12e52f815584d97f7df5cff2373850ac024f4853
hantabaru1014 <hantabaru1014@gmail.com>
2022-01-30 10:27:13 +0900

    bug fix?

--- Assets/kensho/Scripts/LinearPingPongMove.cs
+++ Assets/kensho/Scripts/LinearPingPongMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LinearMoveFloor : MonoBehaviour
+public class LinearPingPongMove : MonoBehaviour
 {
     public bool m_isActive = true;
     public Transform m_point1;
@@ -21,13 +21,16 @@ public class LinearMoveFloor : MonoBehaviour
         }
     }
 
-    private Vector3 moveVec;
+    private Vector2 moveVec;
     private int direction = 1;
+    private Rigidbody2D rig2d;
 
     void Start()
     {
+        rig2d = GetComponent<Rigidbody2D>();
         var diff = m_point2.position - m_point1.position;
-        moveVec = diff / Mathf.Max(diff.x, diff.y);
+        var vec3 = diff / Mathf.Max(diff.x, diff.y);
+        moveVec = new Vector2(vec3.x, vec3.y);
         transform.position = m_point1.position;
     }
 
@@ -35,27 +38,12 @@ public class LinearMoveFloor : MonoBehaviour
     {
         if (IsActive)
         {
-            transform.position += moveVec * m_moveSpeed * direction * Time.deltaTime;
+            rig2d.velocity = moveVec * m_moveSpeed * direction;
             var goalPoint = direction == 1 ? m_point2 : m_point1;
-            if (Vector3.Distance(transform.position, goalPoint.position) <= 0.2)
+            if (Vector3.Distance(transform.position, goalPoint.position) <= 0.3)
             {
                 direction *= -1;
             }
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-            collision.transform.SetParent(transform);
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-            collision.transform.SetParent(null);
         }
     }
 }
---
+++ Assets/kensho/Scripts/PlayerParenter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerParenter : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
