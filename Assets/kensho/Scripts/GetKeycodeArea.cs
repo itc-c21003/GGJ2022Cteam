19c16f66c3e3fe781c9a2cca557074a0e7b86b34
hantabaru1014 <hantabaru1014@gmail.com>
2022-01-30 17:00:34 +0900

    ステージを作成

--- Assets/kensho/Scripts/GetKeycodeArea.cs
+++ Assets/kensho/Scripts/GetKeycodeArea.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class GetKeycodeArea : MonoBehaviour
 {
     public string m_keyName = "DefaultKey";
+    public GameObject m_keyVisual;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" || collision.tag == "Ghost")
         {
             StageStateRegistry.Instance.AddCollectedKey(m_keyName);
+            m_keyVisual.SetActive(false);
             Debug.Log($"Collected Key:{m_keyName}!!");
         }
     }
